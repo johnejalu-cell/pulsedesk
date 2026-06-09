324cf98aed3cf969cfa99d03b802a44877465d78
johnejalu-cell <john.ejalu@gmail.com>
2026-06-10 01:08:24 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,34 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: '*.supabase.co' },
-      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
-    ],
-  },
+
+  typescript: {
+
+    ignoreBuildErrors: true,
+
+  },
+
+  eslint: {
+
+    ignoreDuringBuilds: true,
+
+  },
+
+  images: {
+
+    remotePatterns: [
+
+      { protocol: 'https', hostname: '*.supabase.co' },
+
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+
+    ],
+
+  },
+
 };
 
+
 module.exports = nextConfig;
+
+
